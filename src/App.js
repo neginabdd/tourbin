6578a464acfdeeff743618540c66114110499df3
Negin <negin15.abd@gmail.com>
2024-08-15 11:20:53 +0330

    farsi language added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-
+import logIn from "./logIn";
+import singUP from "./signUp";
 function App() {
   return (
     <div className="App">
---
+++ src/logIn.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function logIn(){
+    return(
+        <div className="LogIn">
+
+        </div>
+    )
+}
