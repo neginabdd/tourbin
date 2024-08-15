6354e45f638431c5ea79bb2d29e727b501c73930
Negin <negin15.abd@gmail.com>
2024-08-15 20:25:00 +0330

    background img added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import Login from './Login.js';
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-     <Login/>
+    <div className="App background-image h-screen w-screen bg-cover bg-center bg-no-repeat opacity-80">
+      <Login />
     </div>
   );
 }
+
 export default App;
--- src/logIn.js
+++ src/logIn.js
@@ -1,18 +1,18 @@
 import React from "react";
 export default function Login(){
     return(
-        <div className="flex items-center justify-center h-screen ">
-         <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-20">
+        <div className="flex items-center justify-center h-screen">
+         <div className="max-w-md mx-auto rounded-xl shadow-lg overflow-hidden md:max-w-2xl bg-gray-200 p-4">
   <div className="px-6 py-4">
-    <h2 className="text-lg font-bold mr-5 -mb-5">سلام!</h2>
-    <h2 className="text-lg font-bold m-5">
+    <h2 className="text-lg font-bold ">سلام!</h2>
+    <h2 className="text-lg font-bold ">
      ورود به توربین</h2>
     <form>
       <label className="block mb-2">
-      <input type="email" className="m-5 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder:text-gray-400 focus:outline-none focus:border-sky-500  focus:ring-sky-500"  placeholder="ادرس ایمبل" />
+      <input type="email" className="mt-2 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder:text-gray-400 focus:outline-none focus:border-sky-500  focus:ring-sky-500"  placeholder="ادرس ایمبل" />
       </label>
-      <input type="password" className="m-5 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder:text-gray-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500" placeholder="رمز" />
-      <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-10 rounded mr-5">ورود</button>
+      <input type="password" className="mt-3 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder:text-gray-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500" placeholder="رمز" />
+      <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-10 rounded mt-3">ورود</button>
     </form>
   </div>
 </div>
